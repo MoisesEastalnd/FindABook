c1579d872ae1a852ccd19e15ecd11cb007f0a153
moises eastland <dewaynepade95@gmail.com>
2019-07-24 19:51:57 -0500

    created routes and added those routes to server.js

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 const express = require("express");
 const app = express();
-//API Route 
+
+app.use(express.urlencoded({ extend: true}));
+app.use(express.json());
+//Serve static assets (for heroku)
+if (process.env.NODE_ENV ==="production") {
+    app.use(express.static("client/build"));
+}
+
+
+//Defined Routes
+app.use("/api/books/Search", require("./routes/Search"));
+app.use("/api/books/Save", require("./routes/Save"));
+
+
 app.get ("*", (req,res) => {
     res.sendFile(path.join(_dirname, "./client/build/index.html"));
 });
