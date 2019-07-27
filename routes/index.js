a44a4d3b61fbf9be933b529e3b09e28733e9b20a
moises eastland <dewaynepade95@gmail.com>
2019-07-26 22:12:28 -0500

    made api folder

---
+++ routes/index.js
@@ -0,0 +1,14 @@
+const path = require ("path");
+const router = require("express").Router();
+const apiRoutes = require("./api");
+
+
+//api routes
+router.use("/api", apiRoutes);
+
+//if no api routes are hit, send the react app
+router.use(function(req, res) {
+ res.sendFile(path.join(_dirnam, "../client/build/index.html"));
+});
+
+module.exports = router;
